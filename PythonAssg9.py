dc6d336f54e928e2af56703f0c432c20a3b92730
afrag <gungorafra93@gmail.com>
2020-07-06 16:01:27 +0300

    This is Assignment1.

---
+++ PythonAssg9.py
@@ -0,0 +1,8 @@
+your_name = "Afra" 
+
+name = input("Please enter your name: ").title().strip()
+
+if name == your_name:
+    print("Hello Afra! Here is your password: AqWs3-")
+else : 
+    print(f"Hello {name}. See you later.")
